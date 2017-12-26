0793ad70a9467754c318dbec1327915116c8f984
hameedexplorer <hameedexplorer@gmail.com>
2017-12-26 22:02:57 +0530

    added new test cases

---
+++ src/demo/TC1.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TC1 {
+
+	@Test
+	public void applicationLogin(){
+		System.out.println("Welcome to Jenkins");
+	}
+	
+}
---
+++ src/demo/TC2.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TC2 {
+
+	@Test
+	public void appLogout(){
+		System.out.println("Log out from Jenkins");
+	}
+}
